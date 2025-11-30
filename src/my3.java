79d0fe5e3bdacd6e6f16e59b94e73d5528b6189c
xboxo <Ponomarevnikita594@gmail.com>
2025-11-30 19:21:37 +0300

    1

--- src/my3.java
+++ src/my3.java
@@ -1,28 +1,50 @@
-void main() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("ax^2 + bx + c = 0");
-    System.out.print("Введите первый коэффицент: " );
-    Double a = scanner.nextDouble();
-    System.out.print("Введите второй коэффицент: "  );
-    Double b = scanner.nextDouble();
-    double b1 = Math.pow(b , 2);
-        System.out.print("Введите третий коэффицент: "  );
-    Double c = scanner.nextDouble();
-    double disc = b1-4*a*c;
-    if (disc > 0) {
-        double x_1 = -b +Math.sqrt(disc) / 2 * a;
-        double x_2 = -b -Math.sqrt(disc) /2 * a;
-        System.out.println("Два корня:" + x_1 + "," + x_2);
+import java.util.Scanner;
 
-    }if (disc == 0 ) {
-        double x1 = -b +Math.sqrt(disc) /2 * a;
-        System.out.println("Один корень: " + x1);
-    }if (disc < 0) {
-        System.out.println("Нет корней");
+class my3 {
+    static void choice1() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Добро пожаловать в калькулятор");
+        System.out.print("""
+                Выберите  действие: (1.+) (2.-) (3.*) (4./)
+                Выбор:  
+                """);
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            System.out.println("Первое число: ");
+            int a = scanner.nextInt();
+            System.out.println("Второе число: ");
+            int b = scanner.nextInt();
+            int sum = a + b;
+            System.out.println("Ответ: " + sum);
+        }
+        if (choice == 2) {
+            System.out.println("Первое число: ");
+            int a1 = scanner.nextInt();
+            System.out.println("Второе число: ");
+            int b1 = scanner.nextInt();
+            int sum1 = a1 - b1;
+            System.out.println("Ответ: " + sum1);
+        }
+        if (choice == 3) {
+            System.out.println("Первое число: ");
+            double a2 = scanner.nextInt();
+            System.out.println("Второе число: ");
+            double b2 = scanner.nextInt();
+            double sum2 = a2 / b2;
+            System.out.println("Ответ: " + sum2);
+        }
+        if (choice == 4) {
+            System.out.println("Первое число: ");
+            int a3 = scanner.nextInt();
+            System.out.println("Второе число: ");
+            int b3 = scanner.nextInt();
+            int sum3 = a3 * b3;
+            System.out.println("Ответ: " + sum3);
+
+        }
     }
 
-
-
-
-
+    void main() {
+        choice1();
+    }
 }
